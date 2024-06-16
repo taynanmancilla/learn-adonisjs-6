ef1f55b16467e5f25424baa227d1bc9e2932ace5
Taynan <taynan.estudo@gmail.com>
2024-06-15 21:51:24 -0400

    feat: [2] Link e Parametros de Rota

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,19 @@
 |
 */
 
+import app from '@adonisjs/core/services/app';
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+import fs from 'node:fs/promises';
+
+router.on('/').render('pages/home').as('home');
+
+router.get('/movies/:slug', async (ctx) => {
+
+    const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`);
+
+    const movie = await fs.readFile(url, 'utf8');
+
+    return ctx.view.render('pages/movies/show', { movie });
+
+}).as('movies.show');
